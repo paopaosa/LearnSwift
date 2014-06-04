cfab897eae07bb8975eb3cf961b9632069373200
Paopao sa <paopaosa@qq.com>
2014-06-04 11:58:21 +0800

    Initial Commit

---
+++ learnSwift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  learnSwift
+//
+//  Created by macpps on 14-6-4.
+//  Copyright (c) 2014年 paopaosa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
